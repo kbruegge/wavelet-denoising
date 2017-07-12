Add correct background calculation (similar to gammapy, not consistent with ctools), add crab spectrum used in ctools

crab_transient_analysis.py
@@ -50,17 +50,16 @@ def main(
     '''
 
     cta_perf_fits = fits.open('/home/lena/Dokumente/CTA/prod3b-caldb-20170502/caldb/data/cta/prod3b/bcf/South_z20_100s/irf_file.fits')
-    data_A_eff = cta_perf_fits[1]
-    data_ang_res = cta_perf_fits[2]
-    data_bg_rate = cta_perf_fits[4]
+    data_A_eff = cta_perf_fits['EFFECTIVE AREA']
+    data_ang_res = cta_perf_fits['POINT SPREAD FUNCTION']
+    data_bg_rate = cta_perf_fits['BACKGROUND']
 
-    a_eff_cta_south = pd.DataFrame(OrderedDict({"E_TeV": (data_A_eff.data['ENERG_LO'][0] + data_A_eff.data['ENERG_HI'][0])/2, "A_eff": data_A_eff.data['EFFAREA'][0].mean(axis=0)}))
+    a_eff_cta_south = pd.DataFrame(OrderedDict({"E_TeV": (data_A_eff.data['ENERG_LO'][0] + data_A_eff.data['ENERG_HI'][0])/2, "A_eff": data_A_eff.data['EFFAREA'][0][0]}))
     ang_res_cta_south = pd.DataFrame(OrderedDict({"E_TeV": (data_ang_res.data['ENERG_LO'][0] + data_ang_res.data['ENERG_HI'][0])/2, "Ang_Res": data_ang_res.data['SIGMA'][0][0]}))
-    bg_rate_south = pd.DataFrame(OrderedDict({"E_TeV": (data_bg_rate.data['ENERG_LO'][0] + data_bg_rate.data['ENERG_HI'][0])/2, "bg_rate": data_bg_rate.data['BGD'][0].sum(axis=1).sum(axis=1)}))
 
     # create cubes for steady source and steady source with transient
-    cube_steady = simulate_steady_source(6 * u.deg, 6 * u.deg, a_eff_cta_south, bg_rate_south, ang_res_cta_south, num_slices=time_steps, time_per_slice=time_per_slice * u.s)
-    cube_with_transient = simulate_steady_source_with_transient(6 * u.deg, 6 * u.deg, 2 * u.deg, 2 * u.deg, a_eff_cta_south, bg_rate_south, ang_res_cta_south, num_slices=time_steps, time_per_slice=time_per_slice * u.s)
+    cube_steady = simulate_steady_source(6 * u.deg, 6 * u.deg, a_eff_cta_south, data_bg_rate, ang_res_cta_south, num_slices=time_steps, time_per_slice=time_per_slice * u.s)
+    cube_with_transient = simulate_steady_source_with_transient(6 * u.deg, 6 * u.deg, 2 * u.deg, 2 * u.deg, a_eff_cta_south, data_bg_rate, ang_res_cta_south, num_slices=time_steps, time_per_slice=time_per_slice * u.s)
 
     # remove mean measured noise from current cube
     cube = cube_with_transient - cube_steady.mean(axis=0)

ctawave/toy_models_crab.py
@@ -1,11 +1,9 @@
-
 import numpy as np
 import astropy.units as u
 import spectrum
 import performance
-from scipy import integrate, signal
 from tqdm import tqdm
-from IPython import embed
+from scipy import signal
 
 
 def simulate_steady_source_with_transient(
@@ -14,7 +12,7 @@ def simulate_steady_source_with_transient(
             x_pos_transient,
             y_pos_transient,
             df_A_eff,
-            df_bg_rate,
+            fits_bg_rate,
             df_Ang_Res,
             num_slices=100,
             time_per_slice=30 * u.s,
@@ -29,28 +27,23 @@ def simulate_steady_source_with_transient(
     sim_area = 2*np.pi*(2*cta_radius)**2
 
     N_steady_source = spectrum.number_particles_crab(time_per_slice, E_min, E_max, sim_area)
-    N_background_cta = int(df_bg_rate.bg_rate.sum() / (u.s) * time_per_slice)
-    sample_factor_a_eff = performance.calc_a_eff_factor(df_A_eff, sim_area)
+    N_background_cta = performance.integrate_background(fits_bg_rate, time_per_slice)
 
-    ### ctools ###
-    # N_steady_source = 621
-    # N_background_cta = 11498
-    # sample_factor_a_eff = 0.355877616747182
+    N_transient_max = 2*N_steady_source                                         # Random number for transient sample!!!
+    transient_scale = (N_transient_max*signal.gaussian(num_slices, std=5)).astype(int)  # arbitrary value for std!!
 
-    # N_transient_max = 2*N_steady_source                                         # Random number for transient sample!!!
-    # transient_scale = (N_transient_max*signal.gaussian(num_slices, std=5)).astype(int)  # arbitrary value for std!!
+    # pks_data = np.loadtxt('/home/lena/Dokumente/CTA/transient_data_2.txt')
+    # flare_interp = np.interp(range(140), pks_data[:, 0] - pks_data[:, 0].min(),  pks_data[:, 1])
+    # transient_scale = (flare_interp/0.25 * N_steady_source).astype(int)
 
-    pks_data = np.loadtxt('/home/lena/Dokumente/CTA/transient_data_2.txt')
-    flare_interp = np.interp(range(140), pks_data[:, 0] - pks_data[:, 0].min(),  pks_data[:, 1])
-    transient_scale = int(flare_interp/0.25*N_steady_source)
     slices = []
     for i in tqdm(range(num_slices)):
-        folded_events_crab = performance.response(time_per_slice, N_steady_source, E_min, E_max, df_A_eff, sample_factor_a_eff, sim_area)
+        folded_events_crab = performance.response(time_per_slice, N_steady_source, E_min, E_max, df_A_eff, sim_area)
         ang_res_steady_source = performance.interp_ang_res(folded_events_crab, df_Ang_Res)
         RA_crab, DEC_crab = performance.sample_positions_steady_source(x_pos_steady_source, y_pos_steady_source, ang_res_steady_source)
         RA_bg, DEC_bg = performance.sample_positions_background_random(fov_min, fov_max, int(N_background_cta))
         if transient_scale[i] > 0:
-            folded_events_transient = performance.response(time_per_slice, transient_scale[i], E_min, E_max, df_A_eff, sample_factor_a_eff, sim_area)
+            folded_events_transient = performance.response(time_per_slice, transient_scale[i], E_min, E_max, df_A_eff, sim_area)
             ang_res_transinet = performance.interp_ang_res(folded_events_transient, df_Ang_Res)
             RA_tr, DEC_tr = performance.sample_positions_steady_source(x_pos_transient, y_pos_transient, ang_res_transinet)
         else:
@@ -67,7 +60,7 @@ def simulate_steady_source(
             x_pos,
             y_pos,
             df_A_eff,
-            df_bg_rate,
+            fits_bg_rate,
             df_Ang_Res,
             num_slices=100,
             time_per_slice=30 * u.s,
@@ -81,19 +74,13 @@ def simulate_steady_source(
     sim_area = 2*np.pi*(2*cta_radius)**2
 
     N_steady_source = spectrum.number_particles_crab(time_per_slice, E_min, E_max, sim_area)
-    N_background_cta = int(df_bg_rate.bg_rate.sum() / (u.s) * time_per_slice)
-    sample_factor_a_eff = performance.calc_a_eff_factor(df_A_eff, sim_area)
+    N_background_cta = performance.integrate_background(fits_bg_rate, time_per_slice)
 
-
-    ### ctools ###
-    # N_steady_source = 621
-    # N_background_cta = 11498
-    # sample_factor_a_eff = 0.355877616747182
-
-    print(N_background_cta, N_steady_source, sample_factor_a_eff, sample_factor_a_eff*N_steady_source)
+    n_events = 0
+    print(N_background_cta, N_steady_source)
     slices = []
     for i in tqdm(range(num_slices)):
-        folded_events_crab = performance.response(time_per_slice, N_steady_source, E_min, E_max, df_A_eff, sample_factor_a_eff, sim_area)
+        folded_events_crab = performance.response(time_per_slice, N_steady_source, E_min, E_max, df_A_eff, sim_area)
         ang_res_steady_source = performance.interp_ang_res(folded_events_crab, df_Ang_Res)
 
         RA_crab, DEC_crab = performance.sample_positions_steady_source(x_pos, y_pos, ang_res_steady_source)
@@ -102,5 +89,6 @@ def simulate_steady_source(
         DEC = np.concatenate([DEC_bg, DEC_crab])
 
         slices.append(np.histogram2d(RA, DEC, range=[[fov_min / u.deg, fov_max / u.deg], [fov_min / u.deg, fov_max / u.deg]], bins=bins)[0])
-
+        n_events += 1/float(num_slices)*len(folded_events_crab)
+    print("Events Mean: ", n_events)
     return np.array(slices)

performance.py
@@ -1,9 +1,8 @@
 import numpy as np
 import spectrum
 import astropy.units as u
-from scipy import integrate, signal
-from tqdm import tqdm
-from IPython import embed
+from scipy import integrate
+
 
 def interp_ang_res(E_TeV, df_Ang_Res):
     '''
@@ -16,6 +15,8 @@ def calc_a_eff_factor(df_A_eff, simulation_area):
     '''
     Returns integrated effective area divided by full impact area
     '''
+
+    simulation_area = simulation_area.to(u.m**2)
     integrate_a_eff = integrate.simps(y=df_A_eff.A_eff, x=df_A_eff.E_TeV) * u.m**2
     integrate_a_impact = simulation_area*(df_A_eff.E_TeV.max() - df_A_eff.E_TeV.min())
     return integrate_a_eff/integrate_a_impact
@@ -28,7 +29,7 @@ def interp_eff_area(E_TeV, df_A_eff):
     return np.interp(E_TeV / u.TeV, df_A_eff.E_TeV, df_A_eff.A_eff)
 
 
-def response(T, N, e_min, e_max, A_eff, sample_factor, simulation_area):
+def response(T, N, e_min, e_max, A_eff, simulation_area):
     '''
     Returns array of events from a source with power law distribution folded with effective area of the telescope
 
@@ -38,24 +39,18 @@ def response(T, N, e_min, e_max, A_eff, sample_factor, simulation_area):
     e_max: upper energy bound
     N: number of events coming from source
     A_eff: DataFrame containing values for effective area
-    sample_factor: integrated effective area divided by impact area
     '''
 
+    simulation_area = simulation_area.to(u.m**2)
     events = spectrum.random_power(e_min, e_max, N)
     folded_events = []
     if len(events) > 0:
-        # A_effs = []
         for e in events:
-            # A_effs.append(interp_eff_area(e, A_eff))
             a_eff_event = interp_eff_area(e, A_eff)
             ulimite = (a_eff_event * u.m**2) / (simulation_area)
 
             if(ulimite.value >= np.random.uniform(0, 1)):
                 folded_events.append(e/u.TeV)
-        # folded_events_l = np.random.choice(a=events, p=np.divide(A_effs, sum(A_effs)), size=int(sample_factor*N))
-    # else:
-    #     folded_events = []
-    # print(len(folded_events), len(folded_events_l))
 
     return folded_events
 
@@ -83,3 +78,25 @@ def sample_positions_background_random(fov_min, fov_max, N):
     RA_bg = np.random.uniform(fov_min / u.deg, fov_max / u.deg, N)
     DEC_bg = np.random.uniform(fov_min / u.deg, fov_max / u.deg, N)
     return RA_bg, DEC_bg
+
+
+def integrate_background(bkg, obs_time):
+    '''
+    Integrate background events from IRF.
+    fits-file provides background rates in 1/MeV/s/sr, therefore energy units must be adapted!
+    There are different IRFs for different observation times and zenith angles. Here the IRF for
+    20 deg and 100 s is used.
+
+    Parameters:
+    bkg: irf-file containing background cube
+    obs_time: observation time per slice
+    '''
+
+    delta_energy = (bkg.data['ENERG_HI'][0] - bkg.data['ENERG_LO'][0]) * u.TeV
+    delta_x = (bkg.data['DETX_HI'][0] - bkg.data['DETX_LO'][0]) * u.deg
+    delta_y = (bkg.data['DETY_HI'][0] - bkg.data['DETY_LO'][0]) * u.deg
+    delta_energy, delta_y, delta_x = np.meshgrid(delta_energy, delta_y, delta_x, indexing='ij')
+    bin_volume = delta_energy.to(u.MeV) * (delta_y * delta_x).to(u.sr)
+    bg = bkg.data['BGD'][0] * (1/(u.MeV * u.s * u.sr))
+    integral = bg * bin_volume
+    return integral.sum() * obs_time

spectrum.py
@@ -1,8 +1,6 @@
 import numpy as np
 import astropy.units as u
-from scipy import integrate, signal
-from tqdm import tqdm
-from IPython import embed
+
 
 def random_power(e_min, e_max, N):
     '''
@@ -18,26 +16,24 @@ def number_particles_crab(T, e_min, e_max, simulation_area):
     Returns the number of particles arriving from a pointlike source with known power law distribution
 
     Parameters:
-    A: Area
-    C: Constant flux factor
-    index: power lax index
     T: observation time
     e_min: lower energy bound
     e_max: upper energy bound
+    simulation_area: simulated area (twice CTA radius)
+    C: Constant flux factor
+    index: power lax index
     '''
-#     energy_unit = u.TeV
-#     index = 2.62
-#     C = 2.83e-11 / (u.cm**2 * u.s * u.TeV)
-#     E_0 = 1 * u.TeV
 
-    e_min = e_min.to(u.MeV)
-    e_max = e_max.to(u.MeV)
+    # Spectrum HEGRA (2004)
+    # index = 2.62
+    # C = 2.83e-11 / (u.cm**2 * u.s * u.TeV)
+    # E_0 = 1 * u.TeV
+
+    # Spectrum used in ctools, energy in MeV!!!!!!!!
     index = 2.48
     C = 5.7e-16 / (u.cm**2 * u.s * u.MeV)
     E_0 = 0.3e6 * u.MeV
-
-    # cta_radius = (800 * u.m).to(u.cm)
-    # A = 2*np.pi*(2*cta_radius)**2
-    # A_ctools = (1.2e7 * u.m**2).to(u.cm**2)
+    e_min = e_min.to(u.MeV)
+    e_max = e_max.to(u.MeV)
 
     return int((simulation_area.to(u.cm**2)*C*T*E_0**(index)/(1-index))*((e_max)**(1-index) - (e_min)**(1-index)))